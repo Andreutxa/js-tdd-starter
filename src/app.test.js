1dbcb9b8c22924ae5120e250a8e78124fc1a4ba6
Irma <irma@Irmas-MacBook-Pro.local>
2021-02-21 18:17:54 +0100

    fizzbuzz tdd

--- src/app.test.js
+++ src/app.test.js
@@ -1,10 +1,36 @@
-import { Test } from './app';
 import { App } from './app';
 
 describe('App tests', () => {
-  const test = new App();
+  const app = new App();
 
-  it('should say hello and name', () => {
-    expect(test.getName('Jon Snow')).toEqual('Hello Jon Snow');
+  it('should print fizz', () => {
+    const response = app.fizzBuzz(6);
+    expect(response).toBe('fizz');
+  });
+
+  it('should not print fizz', () => {
+    const response = app.fizzBuzz(4);
+    expect(response).not.toBe('fizz');
+  });
+
+  it('should print buzz', () => {
+    const response = app.fizzBuzz(5);
+    expect(response).toBe('buzz');
+  });
+
+  it('should not print buzz', () => {
+    const response = app.fizzBuzz(3);
+    expect(response).not.toBe('buzz');
+  });
+
+  it('should print fizzbuzz', () => {
+    const response = app.fizzBuzz(15);
+    expect(response).toBe('fizzbuzz');
+  });
+
+  it('should print fizzbuzz', () => {
+    const response = app.fizzBuzz(7);
+    expect(response).toBe('7');
   });
 });
+
