94575998bd654cdd04d215c963cd7a0df9a149c4
Andrea Astorga <andrea.astorga95@gmail.com>
2021-03-04 19:54:57 +0100

    Done

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 export class App {
-  fizzBuzz(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
-      return `fizzbuzz`;
-    }
-    else if (number % 3 === 0 ) {
-      return `fizz`;
-    }
-    else if (number % 5 === 0 ) {
-      return `buzz`;
-    }
-    else {
-      return `${number}`
-    }
+
+  fizzBuzz (number) {
+
+      if ( typeof number !== 'number') {
+        return 'The parameter should be a number!';
+      }
+
+      if (number % 3 === 0 && number % 5 === 0) {
+        return 'FizzBuzz';
+      } else if (number % 5 === 0) {
+        return 'Buzz';
+      } else if (number % 3 === 0) {
+        return 'Fizz';
+      } else {
+        return number;
+      }
+    
+
   }
+
 }
--- src/app.test.js
+++ src/app.test.js
@@ -1,36 +1,53 @@
 import { App } from './app';
 
 describe('App tests', () => {
+  
   const app = new App();
 
-  it('should print fizz', () => {
-    const response = app.fizzBuzz(6);
-    expect(response).toBe('fizz');
-  });
-
-  it('should not print fizz', () => {
-    const response = app.fizzBuzz(4);
-    expect(response).not.toBe('fizz');
-  });
-
-  it('should print buzz', () => {
-    const response = app.fizzBuzz(5);
-    expect(response).toBe('buzz');
-  });
-
-  it('should not print buzz', () => {
+  it ('Should print Fizz', () => {
     const response = app.fizzBuzz(3);
-    expect(response).not.toBe('buzz');
-  });
+    expect(response).toEqual('Fizz')
+  })
 
-  it('should print fizzbuzz', () => {
+  it ('Should print Buzz', () => {
+    const response = app.fizzBuzz(5);
+    expect(response).toBe('Buzz')
+  })
+
+  it ('Should print FizzBuzz', () => {
     const response = app.fizzBuzz(15);
-    expect(response).toBe('fizzbuzz');
-  });
+    expect(response).toBe('FizzBuzz')
+  })
 
-  it('should print fizzbuzz', () => {
-    const response = app.fizzBuzz(7);
-    expect(response).toBe('7');
-  });
-});
+  it ('Should print the number 2', () => {
+    const response = app.fizzBuzz(2);
+    expect(response).toBe(2)
+  })
 
+  it ('Should print => The parameter should be a number!', () => {
+    const response = app.fizzBuzz('apple');
+    expect(response).toBe('The parameter should be a number!')
+  })
+
+  it ('Should print => The parameter should be a number!', () => {
+    const response = app.fizzBuzz(true);
+    expect(response).toBe('The parameter should be a number!')
+  })
+
+  it ('Should not print the number 7', () => {
+    const response = app.fizzBuzz(3);
+    expect(response).not.toBe(3)
+  })
+
+  it ('Should not print Fizz', () => {
+    const response = app.fizzBuzz(17);
+    expect(response).not.toBe('Fizz')
+  })
+
+  it ('Should not print Buzz', () => {
+    const response = app.fizzBuzz(12);
+    expect(response).not.toBe('Buzz')
+  })
+
+
+}) 
